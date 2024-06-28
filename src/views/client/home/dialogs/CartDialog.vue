590d51565236882fc1ed52c05a5fc037302e4e9a
Hallyson <hallysonnobres@gmail.com>
2024-06-28 00:56:22 -0400

    #22 - Adicionar tela do Carrinho - FRONT

--- src/views/client/home/dialogs/CartDialog.vue
+++ src/views/client/home/dialogs/CartDialog.vue
@@ -27,11 +27,11 @@
                     <span>1</span>
                     <v-icon>mdi-plus</v-icon>
                   </div>
-                  <div>
+                  <div class="mt-2 text-subtitle-1">
                     <span>visualizar</span>
-                    <span>excluir</span>
+                    <span class="ml-4">excluir</span>
                   </div>
-                  <div class="text-subtitle-1">R$ 20,00</div>
+                  <div class="text-subtitle-1 mt-2">R$ 20,00</div>
                 </v-col>
               </v-row>
             </v-card>
@@ -54,11 +54,11 @@
                     <span>1</span>
                     <v-icon>mdi-plus</v-icon>
                   </div>
-                  <div>
+                  <div class="mt-2 text-subtitle-1">
                     <span>visualizar</span>
-                    <span>excluir</span>
+                    <span class="ml-4">excluir</span>
                   </div>
-                  <div class="text-subtitle-1">R$ 20,00</div>
+                  <div class="text-subtitle-1 mt-2">R$ 20,00</div>
                 </v-col>
               </v-row>
             </v-card>
@@ -66,9 +66,36 @@
 
         </v-row>
 
-        <v-row align="center" style="height: 11rem">
+        <v-row align="center" style="margin-top: 50px">
           <v-col cols="12" class="text-h5 text-secondary"> Seu Plano </v-col>
         </v-row>
+
+        <v-col cols="12" sm="6" md="6" lg="6">
+            <v-card variant="outlined" class="my-4 pa-4" style="background-color: transparent">
+              <v-row>
+                <v-col cols="12" sm="12" md="4" lg="4">
+                  <v-img
+                    src="https://cdn.irmaospatrocinio.com.br/img/p/1/7/4/1/6/0/174160-large_default.jpg"
+                    style="border: 2px solid #5a2e2e; border-radius: 5px; width: 200px"
+                  />
+                </v-col>
+                <v-col cols="12" sm="12" md="8" lg="8">
+                  <div class="mt-2 text-h6">Nome do Produto</div>
+                  <div class="mt-2 text-subtitle-1">
+                    Quantidade:
+                    <v-icon>mdi-minus</v-icon>
+                    <span>1</span>
+                    <v-icon>mdi-plus</v-icon>
+                  </div>
+                  <div class="mt-2 text-subtitle-1">
+                    <span>visualizar</span>
+                    <span class="ml-4">excluir</span>
+                  </div>
+                  <div class="text-subtitle-1 mt-2">R$ 20,00</div>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-col>
 
         <v-divider color="secondary" :thickness="2" class="border-opacity-50 my-4" />
         <v-row align="center" style="height: 4rem">
